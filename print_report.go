befb045775d7de17b95f5b016bb941ccacb2de87
AbdKaan <abdu.kaan@gmail.com>
2024-12-20 22:08:27 +0300

    print report

--- print_report.go
+++ print_report.go
@@ -1,15 +1,39 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "sort"
+)
+
+type Page struct {
+    URL   string
+    Count int
+}
 
 func printReport(pages map[string]int, baseURL string) {
-	fmt.Println("=============================")
-	fmt.Printf("REPORT for %s\n", baseURL)
-	fmt.Println("=============================")
+    fmt.Println("=============================")
+    fmt.Printf("REPORT for %s\n", baseURL)
+    fmt.Println("=============================")
 
-	
+    sortedPages := sortPages(pages)
 
-	for x, y := range pages {
-		fmt.Printf("Found %v internal links to %v\n", x, y)
-	}
+    for _, page := range sortedPages {
+        fmt.Printf("Found %v internal links to %v\n", page.Count, page.URL)
+    }
+}
+
+func sortPages(pages map[string]int) []Page {
+    pagesSlice := []Page{}
+    for URL, count := range pages {
+        pagesSlice = append(pagesSlice, Page{URL: URL, Count: count})
+    }
+
+    sort.Slice(pagesSlice, func(i, j int) bool {
+        if pagesSlice[i].Count == pagesSlice[j].Count {
+            return pagesSlice[i].URL < pagesSlice[j].URL
+        }
+        return pagesSlice[i].Count > pagesSlice[j].Count
+    })
+
+    return pagesSlice
 }
